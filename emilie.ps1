d1bb8b10873ebbe2b4b9f253e955e412a5bceb28
JalitiQ <contact@jullienl.com>
2026-07-04 11:30:31 +0200

    Init

---
+++ emilie.ps1
@@ -0,0 +1,5 @@
+$client = New-Object System.Net.Sockets.TcpClient("ADRESSE_IP", 9100)
+$stream = $client.GetStream()
+$bytes = [System.Text.Encoding]::ASCII.GetBytes("TEST IMPRESSION`r`n`f")
+$stream.Write($bytes, 0, $bytes.Length)
+$stream.Close(); $client.Close()
